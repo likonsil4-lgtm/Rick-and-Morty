cdf84e01f19e3bada418cb4105a7baac377fd14d
LikonSil <likonsil4@gmail.com>
2026-03-07 12:59:08 +0200

    Добавил новые зависимые. Исправил Domain Repository (убрал Either)

--- lib/domain/repositories/character_repository.dart
+++ lib/domain/repositories/character_repository.dart
@@ -1,7 +1,14 @@
 import '../entities/character.dart';
 
 abstract class CharacterRepository {
-  Future<List<Character>> getCharacters({int page = 1});
+  Future<List<Character>> getCharacters({
+    int page = 1,
+    String? searchQuery,
+    String? status,
+    String? gender,
+  });
+
+  Future<List<Character>> searchCharacters(String query);
   Future<List<Character>> getFavoriteCharacters();
   Future<void> toggleFavorite(Character character);
   Future<bool> isFavorite(int id);
