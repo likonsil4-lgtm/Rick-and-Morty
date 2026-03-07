94a6040dff8b966a4f955a677f99c1b05d981d08
LikonSil <likonsil4@gmail.com>
2026-03-07 12:38:14 +0200

    Исправляем ошибки с темой

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -12,6 +12,7 @@
 import 'package:dio/dio.dart' as _i361;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+import 'package:shared_preferences/shared_preferences.dart' as _i460;
 
 import '../../data/datasources/local/database_helper.dart' as _i380;
 import '../../data/datasources/remote/character_api_service.dart' as _i234;
@@ -33,9 +34,10 @@ extension GetItInjectableX on _i174.GetIt {
       environmentFilter,
     );
     gh.singleton<_i380.DatabaseHelper>(() => _i380.DatabaseHelper());
+    gh.factory<_i473.ThemeCubit>(
+        () => _i473.ThemeCubit(gh<_i460.SharedPreferences>()));
     gh.factory<_i234.CharacterApiService>(
         () => _i234.CharacterApiService(gh<_i361.Dio>()));
-    gh.factory<_i473.ThemeCubit>(() => _i473.ThemeCubit(gh<InvalidType>()));
     gh.factory<_i863.CharacterRepository>(() => _i1071.CharacterRepositoryImpl(
           gh<_i234.CharacterApiService>(),
           gh<_i380.DatabaseHelper>(),
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,39 +1,73 @@
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData get lightTheme => FlexThemeData.light(
-    scheme: FlexScheme.mandyRed,
-    surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-    blendLevel: 7,
-    subThemesData: const FlexSubThemesData(
-      blendOnLevel: 10,
-      blendOnColors: false,
-      useM2StyleDividerInM3: true,
-      cardRadius: 12,
-      popupMenuRadius: 6,
-      inputDecoratorRadius: 20,
-      inputDecoratorUnfocusedHasBorder: false,
-    ),
-    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+  // Цвета из Rick and Morty
+  static const Color _primaryLight = Color(0xFF00BCD4);
+  static const Color _secondaryLight = Color(0xFF8BC34A);
+  static const Color _primaryDark = Color(0xFF0097A7);
+  static const Color _secondaryDark = Color(0xFF689F38);
+
+  static ThemeData get lightTheme => ThemeData(
     useMaterial3: true,
-    swapLegacyOnMaterial3: true,
+    brightness: Brightness.light,
+    colorScheme: const ColorScheme.light(
+      primary: _primaryLight,
+      secondary: _secondaryLight,
+      surface: Colors.white,
+      background: Color(0xFFF5F5F5),
+      onPrimary: Colors.white,
+      onSecondary: Colors.white,
+      onSurface: Colors.black87,
+      onBackground: Colors.black87,
+    ),
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      elevation: 0,
+      backgroundColor: _primaryLight,
+      foregroundColor: Colors.white,
+    ),
+    cardTheme: const CardThemeData(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: _primaryLight,
+      unselectedItemColor: Colors.grey,
+      type: BottomNavigationBarType.fixed,
+    ),
   );
 
-  static ThemeData get darkTheme => FlexThemeData.dark(
-    scheme: FlexScheme.mandyRed,
-    surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-    blendLevel: 13,
-    subThemesData: const FlexSubThemesData(
-      blendOnLevel: 20,
-      useM2StyleDividerInM3: true,
-      cardRadius: 12,
-      popupMenuRadius: 6,
-      inputDecoratorRadius: 20,
-      inputDecoratorUnfocusedHasBorder: false,
-    ),
-    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+  static ThemeData get darkTheme => ThemeData(
     useMaterial3: true,
-    swapLegacyOnMaterial3: true,
+    brightness: Brightness.dark,
+    colorScheme: const ColorScheme.dark(
+      primary: _primaryDark,
+      secondary: _secondaryDark,
+      surface: Color(0xFF1E1E1E),
+      background: Color(0xFF121212),
+      onPrimary: Colors.white,
+      onSecondary: Colors.white,
+      onSurface: Colors.white,
+      onBackground: Colors.white,
+    ),
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      elevation: 0,
+      backgroundColor: Color(0xFF1E1E1E),
+      foregroundColor: Colors.white,
+    ),
+    cardTheme: const CardThemeData(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: _primaryDark,
+      unselectedItemColor: Colors.grey,
+      type: BottomNavigationBarType.fixed,
+    ),
   );
 }
