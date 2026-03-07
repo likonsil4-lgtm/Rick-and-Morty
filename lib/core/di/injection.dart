f995aecd7b04bdf5eb34c555d3cd10a323a25a64
LikonSil <likonsil4@gmail.com>
2026-03-07 14:54:52 +0200

    Исправил inject

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,41 +1,56 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import '../../presentation/blocs/theme/theme_cubit.dart';
+import '../../data/datasources/local/character_local_datasource.dart';
+import '../../data/datasources/local/database_helper.dart';
+import '../../data/datasources/remote/character_api_service.dart';
+import '../../data/repositories/character_repository_impl.dart';
+import '../../domain/repositories/character_repository.dart';
 import '../../presentation/blocs/characters/characters_cubit.dart';
 import '../../presentation/blocs/favorites/favorites_cubit.dart';
+import '../../presentation/blocs/theme/theme_cubit.dart';
+import '../network/network_info.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> configureDependencies() async {
 
-  /// external
-  final prefs = await SharedPreferences.getInstance();
+  // Network Info
+  getIt.registerSingleton<NetworkInfo>(NetworkInfoImpl(getIt<InternetConnectionChecker>()));
 
-  getIt.registerSingleton<SharedPreferences>(prefs);
+  // Database
+  getIt.registerSingleton<DatabaseHelper>(DatabaseHelper());
 
-  getIt.registerSingleton<Dio>(
-    Dio(BaseOptions(
-      baseUrl: "https://rickandmortyapi.com/api",
-    )),
+  // Data sources
+  getIt.registerFactory<CharacterLocalDataSource>(
+        () => CharacterLocalDataSourceImpl(getIt<SharedPreferences>(), getIt<DatabaseHelper>()),
   );
 
-  getIt.registerSingleton<InternetConnectionChecker>(
-    InternetConnectionChecker(),
+  getIt.registerFactory<CharacterApiService>(
+        () => CharacterApiService(getIt<Dio>()),
   );
 
-  /// cubits
-  getIt.registerFactory(
+  // Repository
+  getIt.registerFactory<CharacterRepository>(
+        () => CharacterRepositoryImpl(
+      getIt<CharacterApiService>(),
+      getIt<CharacterLocalDataSource>(),
+      getIt<NetworkInfo>(),
+    ),
+  );
+
+
+  getIt.registerFactory<ThemeCubit>(
         () => ThemeCubit(getIt<SharedPreferences>()),
   );
 
-  getIt.registerFactory(
-        () => CharactersCubit(getIt<Dio>()),
+  getIt.registerFactory<CharactersCubit>(
+        () => CharactersCubit(getIt<CharacterRepository>()),
   );
 
-  getIt.registerFactory(
-        () => FavoritesCubit(getIt<SharedPreferences>()),
+  getIt.registerFactory<FavoritesCubit>(
+        () => FavoritesCubit(getIt<CharacterRepository>()),
   );
 }
