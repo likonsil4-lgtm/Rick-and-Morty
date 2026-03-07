c238eed84fbef690b95792919a2865c6743f83c3
LikonSil <likonsil4@gmail.com>
2026-03-07 12:28:27 +0200

    Правим mainЭ

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'core/di/injection.dart';
+import 'core/theme/app_theme.dart';
+import 'presentation/blocs/characters/characters_cubit.dart';
+import 'presentation/blocs/favorites/favorites_cubit.dart';
+import 'presentation/blocs/theme/theme_cubit.dart';
+import 'presentation/pages/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Инициализация SharedPreferences до DI
+  final prefs = await SharedPreferences.getInstance();
+
+  // Регистрация базовых зависимостей
+  getIt.registerSingleton<SharedPreferences>(prefs);
+  getIt.registerSingleton<Dio>(Dio(BaseOptions(
+    connectTimeout: const Duration(seconds: 5),
+    receiveTimeout: const Duration(seconds: 3),
+  )));
+
+  configureDependencies();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => getIt<ThemeCubit>()),
+        BlocProvider(create: (_) => getIt<CharactersCubit>()),
+        BlocProvider(create: (_) => getIt<FavoritesCubit>()),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, themeMode) {
+          return MaterialApp(
+            title: 'Rick & Morty',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeMode,
+            home: const HomePage(),
+          );
+        },
+      ),
+    );
+  }
+}
