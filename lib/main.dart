00fe615146667793ab5707690abcb1636f40508a
LikonSil <likonsil4@gmail.com>
2026-03-07 13:17:15 +0200

    Поправил main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-//v1
+//v2
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/di/injection.dart';
@@ -14,17 +15,19 @@ import 'presentation/pages/home_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Инициализация SharedPreferences до DI
+  // Инициализация SharedPreferences
   final prefs = await SharedPreferences.getInstance();
 
-  // Регистрация базовых зависимостей
+  // Регистрация зависимостей вручную (без injectable)
   getIt.registerSingleton<SharedPreferences>(prefs);
   getIt.registerSingleton<Dio>(Dio(BaseOptions(
     connectTimeout: const Duration(seconds: 5),
     receiveTimeout: const Duration(seconds: 3),
   )));
+  getIt.registerSingleton<InternetConnectionChecker>(InternetConnectionChecker());
 
-  configureDependencies();
+  // Теперь вызываем configureDependencies
+  await configureDependencies();
 
   runApp(const MyApp());
 }
